280f2310cf552e0df67835593cddfdbade89ea75
home_iMac <kongyunpeng2011@sina.com>
2019-01-30 01:06:57 +0800

    first init

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+
+import NestedGrid from './grid'
+import SimpleExpansionPanel from './imagGrid'
+
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<SimpleExpansionPanel />, document.getElementById('root'));
+ReactDOM.render(<NestedGrid />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/tileData.js
@@ -0,0 +1,27 @@
+import image from './images/countryBtn.png'
+
+const titeData = [
+    {
+        img:image,
+        title:'china',
+        author:"Jay"
+    },
+    {
+        img:image,
+        title:'china',
+        author:"Jay"
+    },
+    {
+        img:image,
+        title:'china',
+        author:"Jay"
+    },
+    {
+        img:image,
+        title:'china',
+        author:"Jay"
+    }
+
+    ];
+export default titeData
+
