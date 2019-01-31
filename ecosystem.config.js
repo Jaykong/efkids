3ce9c5650628017128bfd61a9a004ccdd9159fd4
home_iMac <kongyunpeng2011@sina.com>
2019-02-01 01:19:34 +0800

    add ecosystem by using pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [{
+    name: 'efkids',
+    script: 'app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+ deploy: {
+    production: {
+      user: "ubuntu",
+      host: "ec2-3-17-39-61.us-east-2.compute.amazonaws.com",
+      key: "~/.ssh/home.pem",
+      ref: "origin/master",
+      repo: "git@github.com:Jaykong/efkids.git",
+      path: "/home/ubuntu/efkids",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
+    }
+  }
+};
