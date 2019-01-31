3b1a7163032b8f7b2a637305eda7f3b21fce03d1
home_iMac <kongyunpeng2011@sina.com>
2019-02-01 01:36:49 +0800

    config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'efkids',
-    script: 'app.js',
+    script: './src/index.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
