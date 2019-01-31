29019e4e7984b4df682dd5d9f0f6a122b21d36f7
home_iMac <kongyunpeng2011@sina.com>
2019-02-01 02:09:22 +0800

    add port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,9 +10,11 @@ module.exports = {
     watch: false,
     max_memory_restart: '1G',
     env: {
+        PORT: 3001,
       NODE_ENV: 'development'
     },
     env_production: {
+        PORT: 3001,
       NODE_ENV: 'production'
     }
   }],
