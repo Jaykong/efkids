9864c30a47ca99f60dd8cfafd0de55405e64060a
pencild <pencild@qq.com>
2019-02-13 02:28:32 +0800

    fix wx

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -24,33 +24,28 @@ export default {
       appId: "wxbcac905317a3ba59", // 必填，公众号的唯一标识
       timestamp: 1414587457, // 必填，生成签名的时间戳
       nonceStr: "Wm3WZYTPz0wzccnW", // 必填，生成签名的随机串
-      signature: "5d8033b9d06c9bfebb3f1cab9da3db143f6ee129", // 必填，签名
+      signature: "3f47597dff643a30d3e5cc8d13355ac11d2036b2", // 必填，签名
       jsApiList: ["updateAppMessageShareData", "updateTimelineShareData"] // 必填，需要使用的JS接口列表
     });
 
-    wx.ready(function() {
-      //需在用户可能点击分享按钮前就先调用
-      wx.updateAppMessageShareData({
-        title: "宝贝足迹", // 分享标题
-        desc: "世界这么大，宝贝都去过哪些地方？", // 分享描述
-        link: "http://www.yiwangezan.cn", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-        imgUrl: "", // 分享图标
-        success: function() {
-          // 设置成功
-          console.log("ok1");
-        }
-      });
-
-      //需在用户可能点击分享按钮前就先调用
-      wx.updateTimelineShareData({
-        title: "宝贝足迹", // 分享标题
-        link: "http://www.yiwangezan.cn", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-        imgUrl: "", // 分享图标
-        success: function() {
-          // 设置成功
-          console.log("ok2");
-        }
-      });
+    wx.updateAppMessageShareData({
+      title: "宝贝足迹", // 分享标题
+      desc: "世界这么大，宝贝都去过哪些地方？", // 分享描述
+      link: "http://www.yiwangezan.cn", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      imgUrl: "", // 分享图标
+      success: function() {
+        // 设置成功
+        alert("ok1");
+      }
+    });
+    wx.updateTimelineShareData({
+      title: "宝贝足迹", // 分享标题
+      link: "http://www.yiwangezan.cn", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      imgUrl: "", // 分享图标
+      success: function() {
+        // 设置成功
+        alert("ok2");
+      }
     });
   }
 };
