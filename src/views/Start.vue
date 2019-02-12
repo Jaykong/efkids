b6c9b40234cbe798be5ef43269c863bda3b6e267
pencild <pencild@qq.com>
2019-02-13 01:43:27 +0800

    wx

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -16,6 +16,23 @@
   </div>
 </template>
 
+<script>
+const wx = require("../sdk/jweixin-1.4.0.js");
+
+export default {
+  mounted() {
+    wx.config({
+      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: "wxbcac905317a3ba59", // 必填，公众号的唯一标识
+      timestamp: 1414587457, // 必填，生成签名的时间戳
+      nonceStr: "Wm3WZYTPz0wzccnW", // 必填，生成签名的随机串
+      signature: "5d8033b9d06c9bfebb3f1cab9da3db143f6ee129", // 必填，签名
+      jsApiList: ["updateAppMessageShareData", "updateTimelineShareData"] // 必填，需要使用的JS接口列表
+    });
+  }
+};
+</script>
+
 <style lang="less" scoped>
 .container {
   width: 100vw;
