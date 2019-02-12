f15430300221b2072eb30815f91e29ad69cc4423
pencild <pencild@qq.com>
2019-02-13 02:19:02 +0800

    fix

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -2,14 +2,14 @@
   <div class="container">
     <van-row type="flex">
       <van-col class="cell-logo">
-        <img class="logo" src="../assets/images/efLogo_start.png" />
+        <img class="logo" src="../assets/images/efLogo_start.png">
       </van-col>
     </van-row>
 
     <van-row class="cell-btn" type="flex" justify="center">
       <van-col>
         <router-link to="/selectcountry">
-          <img class="enter-btn" src="../assets/images/enterBtn.png" />
+          <img class="enter-btn" src="../assets/images/enterBtn.png">
         </router-link>
       </van-col>
     </van-row>
@@ -26,6 +26,31 @@ export default {
       nonceStr: "Wm3WZYTPz0wzccnW", // 必填，生成签名的随机串
       signature: "5d8033b9d06c9bfebb3f1cab9da3db143f6ee129", // 必填，签名
       jsApiList: ["updateAppMessageShareData", "updateTimelineShareData"] // 必填，需要使用的JS接口列表
+    });
+
+    wx.ready(function() {
+      //需在用户可能点击分享按钮前就先调用
+      wx.updateAppMessageShareData({
+        title: "宝贝足迹", // 分享标题
+        desc: "世界这么大，宝贝都去过哪些地方？", // 分享描述
+        link: "http://www.yiwangezan.cn", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: "", // 分享图标
+        success: function() {
+          // 设置成功
+          console.log("ok1");
+        }
+      });
+
+      //需在用户可能点击分享按钮前就先调用
+      wx.updateTimelineShareData({
+        title: "宝贝足迹", // 分享标题
+        link: "http://www.yiwangezan.cn", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: "", // 分享图标
+        success: function() {
+          // 设置成功
+          console.log("ok2");
+        }
+      });
     });
   }
 };
