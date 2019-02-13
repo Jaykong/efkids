7ac96656c15b45f6ae450a388455305897053db3
pencild <pencild@qq.com>
2019-02-14 02:54:25 +0800

    fix

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -59,6 +59,9 @@
 </template>
 
 <script>
+import axios from "axios";
+import sha1 from "js-sha1";
+
 import { Toast } from "vant";
 
 export default {
@@ -67,6 +70,51 @@ export default {
       name: "",
       photo: ""
     };
+  },
+  mounted() {
+    this.getSignature(r => {
+      if (r) {
+        const appId = r.appId;
+        const ticket = r.ticket;
+        const noncestr = this.getRandomString(16);
+        const timestamp = new Date().getTime();
+        const url = window.location.href;
+
+        const string1 = `jsapi_ticket=${ticket}&noncestr=${noncestr}&timestamp=${timestamp}&url=${url}`;
+        const signature = sha1(string1);
+
+        wx.config({
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId, // 必填，公众号的唯一标识
+          timestamp, // 必填，生成签名的时间戳
+          nonceStr: noncestr, // 必填，生成签名的随机串
+          signature, // 必填，签名
+          jsApiList: ["onMenuShareAppMessage", "onMenuShareTimeline"] // 必填，需要使用的JS接口列表
+        });
+      }
+    });
+
+    wx.ready(function() {
+      wx.onMenuShareTimeline({
+        title: "宝贝足迹", // 分享标题
+        link: "https://www.mufenggame.com/", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: "https://www.mufenggame.com/loading/loading.png", // 分享图标
+        success: function() {
+          // 用户点击了分享后执行的回调函数
+        }
+      });
+      wx.onMenuShareAppMessage({
+        title: "宝贝足迹", // 分享标题
+        desc: "世界这么大，宝贝都去过哪些地方？", // 分享描述
+        link: "https://www.mufenggame.com/", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: "https://www.mufenggame.com/loading/loading.png", // 分享图标
+        type: "", // 分享类型,music、video或link，不填默认为link
+        dataUrl: "", // 如果type是music或video，则要提供数据链接，默认为空
+        success: function() {
+          // 用户点击了分享后执行的回调函数
+        }
+      });
+    });
   },
   methods: {
     onRead(file) {
@@ -94,6 +142,34 @@ export default {
           duration: 1500
         });
       }
+    },
+    getSignature(callback) {
+      const api = "https://www.mufenggame.com/wxJssdk/getJssdk";
+      // const current_url = window.location.href;
+
+      axios
+        .get(api)
+        .then(function(r) {
+          // console.log(r);
+          if (r.status === 200) {
+            const data = r.data.data;
+            callback(data);
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    getRandomString(len) {
+      len = len || 32;
+      let $chars =
+        "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678"; /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+      let maxPos = $chars.length;
+      let pwd = "";
+      for (let i = 0; i < len; i++) {
+        pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+      }
+      return pwd;
     }
   }
 };
