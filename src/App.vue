9d1774d6c2b1459806bc06c19d7ce99b1801ac98
home_iMac <kongyunpeng2011@sina.com>
2019-02-16 00:59:45 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default {
     MtaH5.init({
       sid: "500670495", //必填，统计用的appid
       cid: "500670497", //如果开启自定义事件，此项目为必填，否则不填
-      autoReport: 1, //是否开启自动上报(1:init完成则上报一次,0:使用pgv方法才上报)
+      autoReport: 0, //是否开启自动上报(1:init完成则上报一次,0:使用pgv方法才上报)
       senseHash: 1, //hash锚点是否进入url统计
       senseQuery: 1, //url参数是否进入url统计
       performanceMonitor: 0, //是否开启性能监控
